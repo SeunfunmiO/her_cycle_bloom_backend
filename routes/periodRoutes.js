a30cde916b49b79a6d93a09ccb634b07b7ececc8
seunfunmi <seunfunmisore89@gmail.com>
2026-02-18 15:40:47 +0100

    Modified period controller

--- routes/periodRoutes.js
+++ routes/periodRoutes.js
@@ -12,7 +12,7 @@ const router = express.Router()
 
 router.post('/create-period-details', protect, savePeriodDetails)
 router.put('/save-entry', protect, saveUserEntry)
-router.put('/get-entry/:id', protect, getSinglePeriodEntry)
+router.get('/get-entry/:id', protect, getSinglePeriodEntry)
 router.get('/get-entries', protect, getEntries)
 router.patch("/period/:id/end", protect, endPeriod)
 
